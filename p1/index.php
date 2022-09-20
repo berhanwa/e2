8f373d760993ff7f389287a3ebf16cad8fb5efc4
Bethlehem Berhanu <beb489@g.harvard.edu>
2022-09-20 04:25:40 +0000

    created new file, moved code out of index.php, added halfdollars to bank.php

--- p1/index.php
+++ p1/index.php
@@ -1,18 +1,5 @@
-<!doctype html>
-<html lang='en'>
+<?php
 
-<head>
-    <title>Project 1</title>
-    <meta charset='utf-8'>
-    <link href=data:, rel=icon>
-</head>
+require 'index-view.php';
 
-<body>
-    <h1>Project 1</h1>
-
-    <h2>Mechanics</h2>
-
-    <ul>
-        <li>Lorem</li>
-    </ul>
-</body>
+?>
