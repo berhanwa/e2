81ccef7248252500603c8000cd1e96935b5d37d3
Bethlehem Berhanu <beb489@g.harvard.edu>
2022-10-12 02:29:31 +0000

    Created practice forms a b and c in the practice folder

---
+++ practice/forms-a/process.php
@@ -0,0 +1,17 @@
+<?php
+
+// var_dump($_GET);
+
+$answer = $_POST['answer'];
+
+$haveAnswer = true;
+
+if ($answer == '') {
+    $haveAnswer = false;
+} else if ($answer == 'pumpkin') {
+    $correct = true;
+} else {
+    $correct = false;
+}
+
+require 'process-view.php';
