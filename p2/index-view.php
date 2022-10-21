4d82ad5272edabae0bc337e392dc896ddacbac77
Bethlehem Berhanu <beb489@g.harvard.edu>
2022-10-21 01:51:42 +0000

    Setting up Project 2

---
+++ p2/index-view.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Project 2</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+
+<body>
+    <h1>Project 2</h1>
+
+    <form method='POST' action='process.php'>
+
+        <button type='submit'>Play</button>
+
+    </form>
+
+</body>
