7718b96288fdf6c4444f67907d40e9deaab984a4
Bethlehem Berhanu <beb489@g.harvard.edu>
2022-11-09 02:23:18 +0000

    Added ZipFoods app based on e2 framework

--- rps/index.php
+++ rps/index.php
@@ -13,5 +13,5 @@ $recentGames = $_SESSION['results']['recentGames'] ?? null;
 
 $_SESSION['results']['lastGame'] = null;
 
-# Each invocation of the `play` method will play and track a new round of player (given move) vs. computer
-Debug::dump($game->play('rock'));
+
+require 'index-view.php';
---
+++ zipfoods/views/contact.blade.php
@@ -0,0 +1,3 @@
+<h2> Support & Contact Info </h2>
+
+Email us at {{ $email}}.
