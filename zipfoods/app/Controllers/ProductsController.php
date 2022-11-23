b4a7795c3c4e85bd3a5d8e6792f54b0218cfdb10
Bethlehem Berhanu <beb489@g.harvard.edu>
2022-11-23 18:26:42 +0000

    Building on to the zipfoods example, working with MySQL

--- zipfoods/app/Controllers/ProductsController.php
+++ zipfoods/app/Controllers/ProductsController.php
@@ -30,12 +30,74 @@ class ProductsController extends Controller
     {
         $sku = $this->app->param('sku');
 
+        if(is_null($sku)){
+             $this->app->redirect('/products');
+        }
+
         $product = $this->productsObj->getBySku($sku);
 
         if(is_null($product)){
-            return $this->app->view('errors/404');
+            return $this->app->view('products/missing');
         }
 
-        return $this->app->view('products/show', ['product' => $product]);
+        $reviewSaved = $this->app->old('reviewSaved');
+
+        return $this->app->view('products/show', [
+            'product' => $product,
+            'reviewSaved' => $reviewSaved
+        ]);
+    }
+
+    public function saveReview()
+    {
+
+        $this->app->validate([
+            'sku' => 'required',
+            'name' => 'required', # Note how multiple validation rules are separated by a |
+            'review' => 'required|minLength:200' # Note that some rules accept paramaters, which follow a :
+        ]);
+
+        $sku = $this->app->input('sku');
+        $name = $this->app->input('name');
+        $review = $this->app->input('review');
+
+
+        # Set up all the variables we need to make a connection
+    $host = $this->app->env('DB_HOST');
+    $database = $this->app->env('DB_NAME');
+    $username = $this->app->env('DB_USERNAME');
+    $password = $this->app->env('DB_PASSWORD');
+    
+    # DSN (Data Source Name) string
+    # contains the information required to connect to the database
+    $dsn = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+
+    # Driver-specific connection options
+    $options = [
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    try {
+        # Create a PDO instance representing a connection to a database
+        $pdo = new \PDO($dsn, $username, $password, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+    $sqlTemplate = "INSERT INTO reviews (name, sku, review) 
+    VALUES (:name, :sku, :review)";
+
+    $values = [
+        'name' => $name,
+        'sku' => $sku,
+        'review' => $review,
+    ];
+
+    $statement = $pdo->prepare($sqlTemplate);
+    $statement->execute($values);
+
+        return $this->app->redirect('/product?sku=' . $sku, ['reviewSaved' => true]);
     }
 }
---
+++ zipfoods/views/products/missing.blade.php
@@ -0,0 +1,11 @@
+@extends('templates/master')
+
+@section('title')
+    Product Not Found
+@endsection
+
+@section('content')
+    <h2>Product Not Found</h2>
+    <p>Sorry, we were not able to find the product you're looking for.</p>
+    <a href='/products'>&larr; Return to all products</a>
+@endsection
