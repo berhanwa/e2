1cbca14e3e038e9bd7a5743572e048e0b271cb7d
Bethlehem Berhanu <beb489@g.harvard.edu>
2022-12-09 03:30:57 +0000

    Established user, computer choice and game logic.

--- p3/app/Controllers/AppController.php
+++ p3/app/Controllers/AppController.php
@@ -8,10 +8,44 @@ class AppController extends Controller
      */
     public function index()
     {
-        $welcomes = ['Welcome', 'Aloha', 'Welkom', 'Bienvenidos', 'Bienvenu', 'Welkomma'];
-        
+        $choice = $this->app->old('choice');
+        $computerDraw = $this->app->old('computerDraw');
+        $won = $this->app->old('won');
+
         return $this->app->view('index', [
-            'welcome' => $welcomes[array_rand($welcomes)]
+            'choice' => $choice,
+            'computerDraw' => $computerDraw,
+            'won' => $won
         ]);
     }
+
+    
+    public function process()
+    {
+        $choice = $this->app->input('choice');
+
+        function drawMove() {
+            $moves = ['rock', 'paper', 'scissor'];
+            return $moves[rand(0,2)];
+    }
+
+        function decideWinner($userDraw, $computerDraw) {
+            if ($userDraw == $computerDraw) {
+                return 'it was a tie. Please try again!';
+            } elseif ($userDraw == 'rock') {
+                return $computerDraw == 'paper' ? 'the computer won. Please try again!' : 'you won!';
+            } elseif ($userDraw == 'paper') {
+                return $computerDraw == 'scissor' ? 'the computer won. Please try again!' : 'you won!';
+            } elseif ($userDraw == 'scissor') {
+                return $computerDraw == 'rock' ? 'the computer won. Please try again!' : 'you won!';
+            }
+        }
+
+        $userDraw = $_POST['choice'];
+        $computerDraw = drawMove();
+        $won = decideWinner($userDraw, $computerDraw);
+
+        return $this->app->redirect('/', ['choice' => $choice, 'computerDraw' => $computerDraw, 'won' => $won]);
+    }
+
 }
--- p3/views/index.blade.php
+++ p3/views/index.blade.php
@@ -3,9 +3,25 @@
 
 @section('content')
     <h2>Instructions</h2>
+
     <ul>
         <li>Pick a move from either Rock, Paper or Scissor.</li>
         <li>A tie is declared if both you and the computer draw the same move.</li>
         <li>Otherwise: Rock beats Scissors, Scissor beats Paper, Paper beats Rock.</li>
     </ul>
+
+    <form method='POST' action='/process'>
+
+        <input type='radio' name='choice' id='rock' value='rock'><label for='rock'>Rock</label>
+        <input type='radio' name='choice' id='paper' value='paper'><label for='paper'>Paper</label>
+        <input type='radio' name='choice' id='scissor' value='scissor'><label for='scissor'>Scissor</label>
+
+        <button type='submit'>Play</button>
+
+    </form>
+
+    @if ($choice)
+        You chose {{ $choice }}, and the computer chose {{ $computerDraw }}.
+        In this round, {{ $won }}
+    @endif
 @endsection
